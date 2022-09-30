1366c1dbba8f04252d453fb5b400e60c6d7f987f
xderfred <78310976+xderfred@users.noreply.github.com>
2022-09-30 23:10:58 +0200

    Adding List Items as innerHTML in javascript

--- scripts/main.js
+++ scripts/main.js
@@ -14,9 +14,9 @@ class project {
 let existingProjects = []
 let plannedProjects = []
 const d = new project()
-const dividendYieldCalculator = new project("Dividend Calculator", "A Small Calculator for DividendYield and Growth.", "/imgs/")
-const porfolioWebsite = new project("Project Portfolio", "A list of planned and existing Projects", "/imgs/")
-const todoNotePlanner = new project("ToDo Note Planner", "A ToDo List with notes and planning", "/imgs/")
+const dividendYieldCalculator = new project("Dividend Calculator", "A Small Calculator for DividendYield and Growth.", "imgs/remainder_icon.png")
+const porfolioWebsite = new project("Project Portfolio", "A list of planned and existing Projects", "imgs/remainder_icon.png")
+const todoNotePlanner = new project("ToDo Note Planner", "A ToDo List with notes and planning", "imgs/remainder_icon.png")
 
 
 existingProjects.push(porfolioWebsite)
@@ -36,10 +36,29 @@ console.log("porfolioWebsite.description", porfolioWebsite.description);
 console.log("porfolioWebsite.pictureSrc", porfolioWebsite.pictureSrc);
 
 existingProjects.forEach((project) => {
-    
-    existingList.innerHTML += "<li> <div class=\"project project-exist\" id=\"project\"> <img src=" + project.pictureSrc + " class=\"project-image\"> <h4 class=\"project-title\" id=\"project-title\">" + project.name + "</h4><p class=\"project-info\" id=\"project-info\">" + project.description + "</p> </div></li>"
+    existingList.innerHTML += `<li class='project project-exist' id='project'>
+                                        <img src='${project.pictureSrc}' class='project-image'>
+                                        <span class='project-texts'>
+                                        <span class='project-title' id='project-title'>
+                                            ${project.name}
+                                        </span>
+                                        <span class='project-info' id='project-info'>
+                                            <i>${project.description}</i>
+                                        </span>
+                                    </span>
+                                </li>`
 })
 
 plannedProjects.forEach((project) => {
-    plannedList.innerHTML += "<li> <div class=\"project project-exist\" id=\"project\"> <img src=" + project.pictureSrc + " class=\"project-image\"> <h4 class=\"project-title\" id=\"project-title\">" + project.name + "</h4><p class=\"project-info\" id=\"project-info\">" + project.description + "</p> </div></li>"
+    plannedList.innerHTML += `<li class='project project-planned' id='project'>
+                                        <img src='${project.pictureSrc}' class='project-image'>
+                                        <span class='project-texts'>
+                                        <span class='project-title' id='project-title'>
+                                            ${project.name}
+                                        </span>
+                                        <span class='project-info' id='project-info'>
+                                            <i>${project.description}</i>
+                                        </span>
+                                    </span>
+                                </li>`
 })
