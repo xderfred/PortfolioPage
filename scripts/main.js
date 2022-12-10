8c3e992ba71efa2d30d63e6935a3a82659c5faf4
xderfred <78310976+xderfred@users.noreply.github.com>
2022-12-10 22:53:28 +0100

    Added remaining planned Projects

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,6 @@ document.addEventListener("scroll", (e) => {
     }
 })
 
-
 class project {
     constructor(name, description, pictureSrc, link) {
         this.name = name
@@ -22,7 +21,6 @@ class project {
         this.link = link
     }
     
-
     name = ""
     description = ""
     pictureSrc = ""
@@ -31,28 +29,31 @@ class project {
 
 let existingProjects = []
 let plannedProjects = []
-const portfolioWebsite = new project("Project Portfolio", "A list of planned and existing Projects", "imgs/remainder_icon.png", "file:///C:/Users/Frederik/Documents/Programmieren/Projekte/Web/myWebsites/PortfolioPage/index.html")
-const dividendYieldCalculator = new project("Dividend Calculator", "A Small Calculator for DividendYield and Growth.", "imgs/remainder_icon.png", "sites/dividendYieldCalculator.html")
-const todoNotePlanner = new project("ToDo Note Planner", "A ToDo List with notes and planning", "imgs/remainder_icon.png", "sites/todoNotePlanner.html")
-const gameCollection = new project("Game Collection", "A collection of some fun games, e.g. Tetris", "imgs/remainder_icon.png", "sites/todoNotePlanner.html")
+existingProjects.push(new project("Project Portfolio", "A list of planned and existing Projects", "imgs/remainder_icon.png", "file:///C:/Users/Frederik/Documents/Programmieren/Projekte/Web/myWebsites/PortfolioPage/index.html"))
+plannedProjects.push(new project("Dividend Calculator", "A Small Calculator for DividendYield and Growth.", "imgs/remainder_icon.png", "sites/dividendYieldCalculator.html"))
+plannedProjects.push(new project("ToDo Note Planner", "A ToDo List with notes and planning", "imgs/remainder_icon.png", "sites/todoNotePlanner.html"))
+plannedProjects.push(new project("Game Collection", "A collection of some fun games, e.g. Tetris", "imgs/remainder_icon.png", "sites/todoNotePlanner.html"))
+plannedProjects.push(new project("GuitarTabs", "A Collection of Tabs that i want to learn on Guitar", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("DepotRebalancing", "A Overview of all assets and needed rebalancing", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("QuizApp", "A small but fun Quiz", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("QRCodeGenerator", "A Generator for your favorite QR-Codes", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("WeatherApp", "A quick overview over your local Weather Report", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("Online Shop", "A nice and tidy Online Shop", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("Charity Finder", "Find yourself the perfect Charity to donate to", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("TimeZone Tracker", "Keep Track of all the time zones", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("Acronym Finder", "Find Acronyms and discuss about them", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("URL Shortener", "Keep your URLs nice and short", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("Audio Player", "Play your favorite Audio", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("Luxury Brand", "Website of the next big luxurios brand", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("Realtime Chat", "Chat with somebody", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("Movie Database", "A collection of your favorite movies", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("Currency and Measurement Converter", "Calculate between different Currencies and Measurements", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("Calculator", "A basic calculator", "imgs/remainder_icon.png", ""))
+plannedProjects.push(new project("ToDo List", "A quick and simple ToDo List", "imgs/remainder_icon.png", ""))
 
-
-existingProjects.push(portfolioWebsite)
-plannedProjects.push(dividendYieldCalculator)
-plannedProjects.push(todoNotePlanner)
-plannedProjects.push(gameCollection)
 
 var existingList = document.getElementById("existing-list")
 var plannedList = document.getElementById("planned-list")
-
-var entry = document.getElementById("project")
-var info = document.getElementById("project-info")
-var title = document.getElementById("project-title")
-
-console.log("portfolioWebsite", portfolioWebsite);
-console.log("portfolioWebsite.name", portfolioWebsite.name);
-console.log("portfolioWebsite.description", portfolioWebsite.description);
-console.log("portfolioWebsite.pictureSrc", portfolioWebsite.pictureSrc);
 
 existingProjects.forEach((project) => {
     existingList.innerHTML += `<li class='project project-exist' id='project' onclick='window.location = "${project.link}"'>
